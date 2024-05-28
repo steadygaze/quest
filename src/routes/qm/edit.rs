66ccb459e1ae80fc7d7249ae66010d3295f6a63b
Steady Gaze <119086591+steadygaze@users.noreply.github.com>
2024-05-28 10:10:31 -0700

    save progress

--- src/routes/qm/edit.rs
+++ src/routes/qm/edit.rs
@@ -2,9 +2,14 @@ use actix_web::dev::ServiceFactory;
 use actix_web::dev::ServiceRequest;
 use actix_web::get;
 use actix_web::post;
+use actix_web::web;
 use actix_web::HttpResponse;
 use actix_web::Responder;
+use askama_actix::Template;
+use askama_actix::TemplateToResponse;
 
+use crate::app_state::AppConfig;
+use crate::app_state::AppState;
 use crate::error::Result;
 
 pub fn add_routes<T>(app: actix_web::App<T>) -> actix_web::App<T>
@@ -15,9 +20,18 @@ where
         .service(create_new_quest_submit)
 }
 
+#[derive(Template)]
+#[template(path = "create_quest.html")]
+struct CreateQuestTemplate<'a> {
+    config: &'a AppConfig,
+}
+
 #[get("/qm/new")]
-async fn create_new_quest_form() -> Result<impl Responder> {
-    Ok(HttpResponse::Ok().body(""))
+async fn create_new_quest_form(app_state: web::Data<AppState>) -> Result<impl Responder> {
+    Ok(CreateQuestTemplate {
+        config: &app_state.config,
+    }
+    .to_response())
 }
 
 #[post("/qm/new")]
