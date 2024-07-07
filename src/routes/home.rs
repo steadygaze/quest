9d8fc9025372c29dae9bb1185eb8c003be2dfd20
Steady Gaze <119086591+steadygaze@users.noreply.github.com>
2024-07-06 19:09:43 -0700

    Fix home/qm routes

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,10 +1,5 @@
 use crate::routes::prelude::*;
 
-/// Add auth-related routes.
-pub fn add_routes(scope: actix_web::Scope) -> actix_web::Scope {
-    scope.service(index)
-}
-
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate<'a> {
@@ -14,7 +9,7 @@ struct IndexTemplate<'a> {
 }
 
 #[get("/")]
-async fn index(app_state: web::Data<AppState>, request: HttpRequest) -> Result<impl Responder> {
+pub async fn index(app_state: web::Data<AppState>, request: HttpRequest) -> Result<impl Responder> {
     let session_info_option = app_state.get_session(request).await.transpose()?;
     let logged_in = session_info_option.is_some();
     let current_profile = &session_info_option.and_then(|x| x.current_profile);
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -13,7 +13,9 @@ where
     T: ServiceFactory<ServiceRequest, Config = (), Error = actix_web::Error, InitError = ()>,
 {
     app.service(auth::add_routes(web::scope("/auth")))
-        .service(home::add_routes(web::scope("")))
         .service(qm::add_routes(web::scope("/qm")))
         .service(quest::add_routes(web::scope("/@{username}")))
+        // We have to add the home route individually as a special exception because web::scope("")
+        // interferes with other "/" routes for some reason.
+        .service(home::index)
 }
