89826c1adbdd8f4be3d19ba600d915f519264ad5
Steady Gaze <119086591+steadygaze@users.noreply.github.com>
2024-08-09 00:35:33 -0400

    Add check if username is taken to settings

--- static/js/defer.js
+++ static/js/defer.js
@@ -5,3 +5,26 @@ function usernameify(str) {
     .toLowerCase()
     .replaceAll(/[^a-z0-9]/g, "");
 }
+
+function check_username(event) {
+  const username = event.detail.requestConfig.parameters.username;
+  if (globalThis.username && globalThis.username === username) {
+    // Prevent double-triggering in some circumstances.
+    event.preventDefault(); // Cancel AJAX.
+    return;
+  }
+  globalThis.username = username;
+  if (username.length < 3) {
+    event.preventDefault(); // Cancel AJAX.
+    document.getElementById("username-validation-target").innerHTML =
+      "<span class='text-amber-600'>Username too short</span>";
+  } else if (/[^a-z0-9]/.test(username)) {
+    event.preventDefault(); // Cancel AJAX.
+    document.getElementById("username-validation-target").innerHTML =
+      "<span class='text-amber-600'>Username contains unallowed characters</span>";
+  } else if (/^[^a-z]/.test(username)) {
+    event.preventDefault(); // Cancel AJAX.
+    document.getElementById("username-validation-target").innerHTML =
+      "<span class='text-amber-600'>Username must start with a letter (not a number)</span>";
+  }
+}
